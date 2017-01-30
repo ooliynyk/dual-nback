6a4b6cdffa5b2cc58fe63f21a262c6073f4e5d05
ooliynyk <o.p.oliynyk@gmail.com>
2017-01-30 23:18:42 +0200

    Started session controller implementation

--- src/main/java/justforfun/dualnback/DualNbackApplication.java
+++ src/main/java/justforfun/dualnback/DualNbackApplication.java
@@ -1,16 +1,25 @@
 package justforfun.dualnback;
 
+import java.io.IOException;
 import java.util.Scanner;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import justforfun.dualnback.core.DualNBackSession;
 import justforfun.dualnback.core.GameConfiguration;
 import justforfun.dualnback.core.SessionScore;
 import justforfun.dualnback.core.SessionStateListener;
 import justforfun.dualnback.core.TrialState;
 
-public class DualNbackApplication {
+public class DualNbackApplication extends Application {
 
-	public static void main(String[] args) throws InterruptedException {
+	private Stage primaryStage;
+	private AnchorPane rootLayout;
+
+	public static void test(String[] args) throws InterruptedException {
 		DualNBackSession session = new DualNBackSession(new GameConfiguration(2, 10, 3));
 		session.addStateListener(new SessionStateListener() {
 
@@ -36,6 +45,32 @@ public class DualNbackApplication {
 					System.out.println(session.isCurrentLetterAsNBack());
 				}
 			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Double N-Back");
+		initSessionScene();
+	}
+
+	public void initSessionScene() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(DualNbackApplication.class.getResource("ui/Session.fxml"));
+
+			rootLayout = (AnchorPane) loader.load();
+
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
